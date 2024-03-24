540ab2dd4757d5853bbcdf9e564665cd06a69281
Ludovic28 <148745476+Ludovic28@users.noreply.github.com>
2024-03-24 21:22:04 +0100

    test music

--- BigSwordRPG-C#/Music.cs
+++ BigSwordRPG-C#/Music.cs
@@ -9,22 +9,74 @@ namespace BigSwordRPG_C_
     public class Music
     {
         private string test;
-        private int[] frequencies;
-        private int[] durations;
+        private int NoteDo;
+        private int NoteRe;
+        private int NoteMi;
+        private int NoteFa;
+        private int NoteSol;
+        private int NoteLa;
+        private int NoteSi;
+        List<string> partition;
 
         public Music()
         {
-            
+            NoteDo = 261;
+            NoteRe = 293;
+            NoteMi = 329;
+            NoteFa = 349;
+            NoteSol = 392;
+            NoteLa = 440;
+            NoteSi = 493;
+
+            partition = new List<string>
+        {
+            "Do", "Fa", "Sol", "Do",
+            "Do", "Fa", "Sol", "Do",
+            "Ré", "Sol", "La", "Ré",
+            "Ré", "Sol", "La", "Ré"
+        };
+
         }
 
         public void PlayBeepSelectMenu()
         {
-            Console.Beep(392, 100);
+            Console.Beep(NoteLa, 100);
         }
 
         public void PlayEnterMenu()
         {
-            Console.Beep(262, 100);
+            Console.Beep(NoteDo, 100);
+        }
+
+        public void PlayMusic()
+        {
+            for (int i = 0; i < partition[i].Length; i++)
+            {
+                switch (partition[i])
+                {
+                    case "Do":
+                        Console.Beep(NoteDo, 1500);
+                        break;
+                    case "Fa":
+                        Console.Beep(NoteFa, 1500);
+                        break;
+                    case "Sol":
+                        Console.Beep(NoteSol, 1500);
+                        break;
+                    case "La":
+                        Console.Beep(NoteLa, 1500);
+                        break;
+                    case "Si":
+                        Console.Beep(NoteSi, 1500);
+                        break;
+                    case "Re":
+                        Console.Beep(NoteRe, 1500);
+                        break;
+                    case "Mi":
+                        Console.Beep(NoteMi, 1500);
+                        break;
+                }
+            }
         }
     }
 }
